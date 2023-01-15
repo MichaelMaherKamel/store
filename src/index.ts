59eb93db68e001128fe8988cdc458efa118a08b8
Mike <michaelmaherkamel@gmail.com>
2023-01-15 21:42:34 +0200

    Adding middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
-import express from 'express'
-
+import express, { request } from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 const port = 3000
 
 // Create instance from server
 const app = express()
 
+//middleware to parse incoming requests
+app.use(express.json())
+
+//HTTP request logger middleware
+app.use(morgan('common'))
+
+//HTTP security middleware
+app.use(helmet())
+
+// Apply the rate limiting middleware to all requests
+app.use(rateLimit({
+	windowMs: 60 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: "Too many requests"
+}))
+
 // Add route for the / path + defining HTTP verb = get
 app.get('/', (req,res) => {
     res.send("Wla wla wla el so7ab yalla 🔥")
+})
+
+//Post request
+app.post('/', (req,res) => {
+    console.log(req.body)
+    res.json(
+
+        {
+            message: 'Hello World from Post',
+            data: req.body
+        }
+    )
 })
 
 // Start express server
